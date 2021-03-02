c970878844edbba055edf5fb53674c75a9296179
howard.reith@gmail.com <howard.reith@gmail.com>
2021-03-02 12:27:13 -0500

    Updates

---
+++ precache-manifest.746bb4865c08a82c57733939d1200cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782c9fad970cf09ae8eddbdce106faa5",
+    "url": "/websocket_demo_frontend/index.html"
+  },
+  {
+    "revision": "f983dc5a01a0b4855071",
+    "url": "/websocket_demo_frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "319c3a1535256e54ce41",
+    "url": "/websocket_demo_frontend/static/js/2.f7ff6dd9.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/websocket_demo_frontend/static/js/2.f7ff6dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f983dc5a01a0b4855071",
+    "url": "/websocket_demo_frontend/static/js/main.a6c5cc8b.chunk.js"
+  },
+  {
+    "revision": "3ad1986d5c84b9997268",
+    "url": "/websocket_demo_frontend/static/js/runtime-main.73c197b2.js"
+  }
+]);
