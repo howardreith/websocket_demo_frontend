99bcb1ac8143305b82ab4ba7b46c59b9e9c85b49
howard.reith@gmail.com <howard.reith@gmail.com>
2021-03-06 11:01:41 -0500

    Updates

--- precache-manifest.86adccea758cd06c2553c10703ad967a.js
+++ precache-manifest.86adccea758cd06c2553c10703ad967a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0aed66d745aa34cea9eee22bfed87235",
+    "revision": "b7a8c4f3380fd1afe188a051d0cbc7e9",
     "url": "/websocket_demo_frontend/index.html"
   },
   {
-    "revision": "c4acd0255fdc87566dfd",
+    "revision": "8bdc9cbf0cb2146995ea",
     "url": "/websocket_demo_frontend/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "ebcfddefc29456a3899c",
-    "url": "/websocket_demo_frontend/static/js/2.45d2b6fc.chunk.js"
+    "revision": "45b38e06ecd9dd92e894",
+    "url": "/websocket_demo_frontend/static/js/2.fced516e.chunk.js"
   },
   {
     "revision": "9b9de423b52b798b8fc649e7d9e276fb",
-    "url": "/websocket_demo_frontend/static/js/2.45d2b6fc.chunk.js.LICENSE.txt"
+    "url": "/websocket_demo_frontend/static/js/2.fced516e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4acd0255fdc87566dfd",
-    "url": "/websocket_demo_frontend/static/js/main.006c7347.chunk.js"
+    "revision": "8bdc9cbf0cb2146995ea",
+    "url": "/websocket_demo_frontend/static/js/main.5acc415b.chunk.js"
   },
   {
     "revision": "3ad1986d5c84b9997268",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/websocket_demo_frontend/precache-manifest.70b813f099be4234cf5daabaf30792ff.js"
+  "/websocket_demo_frontend/precache-manifest.86adccea758cd06c2553c10703ad967a.js"
 );
 
 self.addEventListener('message', (event) => {
